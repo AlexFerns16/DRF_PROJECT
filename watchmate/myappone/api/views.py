9a95ff8977bd7712368329846f91a8f7a48fc8c9
AlexFerns16 <alexfernandes828@gmail.com>
2023-08-26 17:06:59 +0530

    commit:18

--- watchmate/myappone/api/views.py
+++ watchmate/myappone/api/views.py
@@ -10,6 +10,7 @@ from myappone.api.serializers import MovieSerializer
 
 @api_view(['GET', 'POST'])                                            
 def movie_list(request):
+    
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)         
@@ -27,8 +28,21 @@ def movie_list(request):
 # ---------------------------------------------------------------------------------------------------------------------
 # by default: set for 'GET' request
 
-@api_view()                                                 
+@api_view(['GET', 'PUT', 'DELETE'])                                                 
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        pass
